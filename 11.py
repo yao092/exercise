45d2d213215127a968ba7d0167c591984d3cff75
yao092 <102135449+yao092@users.noreply.github.com>
2022-06-21 23:59:37 +0800

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,51 @@
+x,y=input("輸入月及日為:").split();
+x=int(x);y=int(y)
+
+if x==1 and y<=20:
+    print("星座為:Capricorn")
+elif x==1 and y>=21:
+    print("星座為:Aquarius")
+elif x==2 and y<=18:
+    print("星座為:Aquarius")    
+elif x==2 and y>=19 :
+    print("星座為:Pisces")
+elif x==3 and y<=20:
+    print("星座為:Pisces")
+elif x==3 and y>=21 :
+    print("星座為:Aries")
+elif x==4 and y<=20:
+    print("星座為:Aries")  
+elif x==4 and y>=21 :
+    print("星座為:Taurus")
+elif x==5 and y<=21:
+    print("星座為:Taurus")    
+elif x==5 and y>=22 :
+    print("星座為:Gemini")
+elif x==6 and y<=21:
+    print("星座為:Gemini")
+elif x==6 and y>=22 :
+    print("星座為:Cancer")
+elif x==7 and y<=22:
+    print("星座為:Cancer")       
+elif x==7 and y>=23 :
+    print("星座為:Leo")
+elif x==8 and y<=23:
+    print("星座為:Leo")
+elif x==8 and y>=24 :
+    print("星座為:Virgo")
+elif x==9 and y<=23:
+    print("星座為:Virgo")
+elif x==9 and y>=24 :
+    print("星座為:Libra")
+elif x==10 and y<=23:
+    print("星座為:Libra")
+elif x==10 and y>=24 :
+    print("星座為:Scorpio")
+elif x==11 and y<=22:
+    print("星座為:Scorpio")
+elif x==11 and y>=23 :
+    print("星座為:Sagittarius")
+elif x==12 and y<=21:
+    print("星座為:Sagittarius")
+elif x==12 and y>=22 :
+    print("星座為:Capricorn")
